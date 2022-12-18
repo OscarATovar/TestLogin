3f75fb9f57a35136b8f0a23728755cad38459664
OscarATovar <jhucaron@gmail.com>
2022-12-18 18:46:30 -0500

    Arreglo los errores

--- src/Register.jsx
+++ src/Register.jsx
@@ -11,17 +11,17 @@ export const Register = (props) => {
     }
     return(
         <div className="auth-form-container">
-            <h2>Register</h2>
-        <form className="register-form" onSubmit={handleSubmit}>
-            <label htmlFor="name">Full name</label>
-            <input value={name} name="name" id="name" placeholder="full name"/>
-            <label htmlFor="email">email</label>
-            <input value={email} type="email" placeholder="youremail@gmail.com" id="email" name="email"/>
-            <label htmlFor="password">password</label>
-            <input value={pass} type="password" placeholder="********" id="password" name="password"/>
-            <button type="submit">Log in</button>
-        </form>
-        <button className="link-btn" onClick={() => props.onFormSwitch('login')}>Already have an account? Login here.</button>
-        </div>
+        <h2>Register</h2>
+    <form className="register-form" onSubmit={handleSubmit}>
+        <label htmlFor="name">Full name</label>
+        <input value={name} name="name" onChange={(e) => setName(e.target.value)} id="name" placeholder="full Name" />
+        <label htmlFor="email">email</label>
+        <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" />
+        <label htmlFor="password">password</label>
+        <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
+        <button type="submit">Log In</button>
+    </form>
+    <button className="link-btn" onClick={() => props.onFormSwitch('login')}>Already have an account? Login here.</button>
+</div>
     )
 }
